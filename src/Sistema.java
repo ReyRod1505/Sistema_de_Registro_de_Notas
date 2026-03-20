c5654db0746b8eab586067698caec66b878f8a40
ReyRod15 <rod.reynolds1505@gmail.com>
2026-03-20 15:59:47 -0600

    Ejercicio Completo

---
+++ src/Sistema.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Sistema {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el nombre del estudiante: ");
+        String name = sc.nextLine();
+
+        double gradeSum = 0;
+        int gradesNumber = 0;
+        String continuee;
+
+        do {
+            System.out.print("Ingrese una nota (0-100): ");
+            double grade = sc.nextDouble();
+            if (grade >= 0 && grade <= 100)  {
+                gradeSum += grade;
+                gradesNumber++;
+            } else {
+                System.out.println("Nota inválida. Por favor, ingrese una nota entre 0 y 100.");
+            }
+            System.out.print("¿Desea ingresar otra nota? (s/n): ");
+            continuee = sc.next();
+        } while (continuee.equalsIgnoreCase("s"));
+
+        if (gradesNumber > 0) {
+            double average = gradeSum / gradesNumber;
+
+            String estadoAcademico;
+            if (average >= 70) {
+                estadoAcademico = "Aprobado";
+            } else {
+                estadoAcademico = "Reprobado";
+            }
+
+            System.out.println("\nResumen del estudiante:");
+            System.out.println("Nombre: " + name);
+            System.out.println("Promedio: " + average);
+            System.out.println("Estado Académico: " + estadoAcademico);
+        } else {
+            System.out.println("No se ingresaron notas para el estudiante.");
+        }
+
+    }
+}
